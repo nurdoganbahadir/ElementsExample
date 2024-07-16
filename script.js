736b1deedb40bad7b07ea54e456446388b7e6fce
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-16 19:45:17 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const todo = document.querySelector(".list-group");
-const todoList = todo.children[1];
-console.log(todoList.textContent="todo");
+// const todo = document.querySelector(".list-group");
+// const todoList = todo.children[1];
+// console.log(todoList.textContent="todo");
+
+// const todo = document.querySelector(".container");
+// todoList = todo.children[0].children[0].children[3].children[2].children[2];
+// todoList.style.color = "red";
+// todoList.style.backgroundColor = "gray";
+// todoList.style.padding = "100px";
+// console.log(todoList);
+
+// const todo = document.querySelector(".list-group");
+// todo.removeChild(todo.children[0]);
+// console.log(todo);
